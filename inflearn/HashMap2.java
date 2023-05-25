2c1efe311f93233ff8b3c98df91d97b7135e16d9
SH-Kim1999 <ktjrghks0914@gmail.com>
2023-05-25 23:53:58 +0900

    아나그램

---
+++ inflearn/HashMap2.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.*;
+
+class Main {
+    public String solution(String s1, String s2) {
+        String answer = "YES";
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char x : s1.toCharArray()) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+        for (char x : s2.toCharArray()) {
+            if(!map.containsKey(x) || map.get(x)==0){
+                return "NO";
+            }
+            map.put(x,map.get(x)-1);
+
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String s1 = kb.next();
+        String s2 = kb.next();
+
+        System.out.print(T.solution(s1, s2));
+    }
+}
